6d5adacd1af93b3b14a81196a06dc396ef352120
Alleluid <milotig@gmail.com>
2021-02-23 02:05:52 -0800

    Reorganize mortar+pestle logic, implement clicking blocks to process them.

--- src/main/java/net/latenighters/cits/common/items/ItemMortarPestle.java
+++ src/main/java/net/latenighters/cits/common/items/ItemMortarPestle.java
@@ -3,13 +3,18 @@ package net.latenighters.cits.common.items;
 import com.simibubi.create.AllRecipeTypes;
 import com.simibubi.create.content.contraptions.components.millstone.MillingRecipe;
 import net.latenighters.cits.ModSetup;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.items.ItemStackHandler;
 import net.minecraftforge.items.wrapper.RecipeWrapper;
@@ -26,30 +31,56 @@ public class ItemMortarPestle extends Item {
     public ActionResult<ItemStack> onItemRightClick(@Nonnull World worldIn, PlayerEntity playerIn, @Nonnull Hand handIn) {
         // TODO: Polish this up to be like sandpaper.
 
-        ItemStack itemStack = playerIn.getHeldItem(handIn);
-        ActionResult<ItemStack> FAIL = new ActionResult<>(ActionResultType.FAIL, itemStack);
         Hand otherHand = handIn == Hand.MAIN_HAND ? Hand.OFF_HAND : Hand.MAIN_HAND;
         ItemStack itemInOtherHand = playerIn.getHeldItem(otherHand);
 
-        RecipeWrapper recipeWrapper = new MillingInv(itemInOtherHand);
-        Optional<MillingRecipe> recipe = AllRecipeTypes.MILLING.find(recipeWrapper, worldIn);
-        if (!recipe.isPresent()) {
-            return FAIL;
-        } else if (!worldIn.isRemote) {
+        boolean recipeResult = preformRecipe(worldIn, playerIn, itemInOtherHand.getItem());
+        if (recipeResult)
             itemInOtherHand.shrink(1);
-            recipe.get().rollResults().forEach((item) -> {
-                ItemEntity itemEntity = playerIn.dropItem(item, false, true);
-                if (itemEntity != null)
-                    itemEntity.setPickupDelay(5);
-            });
-        }
+
         return super.onItemRightClick(worldIn, playerIn, handIn);
     }
 
+    @Override
+    public ActionResultType onItemUse(@Nonnull ItemUseContext context) {
+        World world = context.getWorld();
+        BlockPos pos = context.getPos();
+        BlockState blockState = world.getBlockState(pos);
+
+        boolean recipeResult = preformRecipe(context);
+
+        if (recipeResult){
+            FluidState fluidState = world.getFluidState(pos);
+            world.playEvent(2001, pos, Block.getStateId(blockState)); // Play block break effects (I think)
+            world.setBlockState(pos, fluidState.getBlockState(), 3); // flags | 1: block update, 2: send to clients. TODO: Block update not working..?
+        }
+        return super.onItemUse(context);
+    }
+
+    private boolean preformRecipe(World worldIn, PlayerEntity playerIn, Item inputItem) {
+        RecipeWrapper recipeWrapper = new MillingInv(inputItem);
+        Optional<MillingRecipe> recipe = AllRecipeTypes.MILLING.find(recipeWrapper, worldIn);
+        if (recipe.isPresent()) {
+            if (!worldIn.isRemote) {
+                recipe.get().rollResults().forEach((item) -> {
+                    ItemEntity itemEntity = new ItemEntity(worldIn, playerIn.getPosX(), playerIn.getPosY() + 0.1, playerIn.getPosZ(), item);
+                    itemEntity.setPickupDelay(0);
+                    worldIn.addEntity(itemEntity);
+                });
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private boolean preformRecipe(ItemUseContext context) {
+        return preformRecipe(context.getWorld(), context.getPlayer(), context.getWorld().getBlockState(context.getPos()).getBlock().asItem());
+    }
+
     private static class MillingInv extends RecipeWrapper {
-        public MillingInv(ItemStack stack) {
+        public MillingInv(Item item) {
             super(new ItemStackHandler(1));
-            inv.setStackInSlot(0, stack);
+            inv.setStackInSlot(0, new ItemStack(item));
         }
     }
 }
